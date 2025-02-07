0fab2ce1fd9934e649c3727abf7367339d8f5e33
thdwoqor <celebarte01@gmail.com>
2025-02-07 17:30:32 +0900

    init commit

---
+++ src/test/java/com/querypie/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.querypie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
