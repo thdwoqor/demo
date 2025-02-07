b113b100a4691e87436d9c59ae85d0f6fa8fc1b3
thdwoqor <celebarte01@gmail.com>
2025-02-07 18:24:23 +0900

    feat: 유저 조회 기능 구현

--- src/main/java/com/querypie/controller/UserController.java
+++ src/main/java/com/querypie/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.querypie.controller;
 
 import com.querypie.service.UserService;
+import com.querypie.service.dto.UserResponse;
 import com.querypie.service.dto.UserSaveRequest;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +19,18 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/users")
-    public ResponseEntity<Void> save(@RequestBody final UserSaveRequest request){
+    public ResponseEntity<Void> save(@RequestBody final UserSaveRequest request) {
         userService.save(request);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserResponse>> findAll() {
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<UserResponse> findById(@PathVariable final Long userId) {
+        return ResponseEntity.ok(userService.findById(userId));
     }
 }
--- src/test/java/com/querypie/controller/UserControllerTest.java
+++ src/test/java/com/querypie/controller/UserControllerTest.java
@@ -1,9 +1,17 @@
 package com.querypie.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.querypie.DatabaseCleaner;
+import com.querypie.domain.User;
+import com.querypie.domain.UserRepository;
+import com.querypie.service.dto.UserResponse;
 import com.querypie.service.dto.UserSaveRequest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +25,8 @@ class UserControllerTest {
 
     @Autowired
     private DatabaseCleaner databaseCleaner;
+    @Autowired
+    private UserRepository userRepository;
 
     @LocalServerPort
     private int port;
@@ -37,5 +47,43 @@ class UserControllerTest {
                 .then().log().all()
                 .statusCode(200)
                 .extract();
+    }
+
+    @Test
+    void 모든_사용자를_조회할_수_있다() {
+        //given
+        User user1 = userRepository.save(new User("송재백"));
+        User user2 = userRepository.save(new User("홍길동"));
+
+        //when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when().get("/users")
+                .then().log().all()
+                .statusCode(200)
+                .extract();
+
+        final List<UserResponse> responses = response.jsonPath().getList(".", UserResponse.class);
+        assertThat(responses).hasSize(2);
+        assertThat(responses.get(0).id()).isEqualTo(user1.getId());
+        assertThat(responses.get(0).name()).isEqualTo(user1.getName());
+        assertThat(responses.get(1).id()).isEqualTo(user2.getId());
+        assertThat(responses.get(1).name()).isEqualTo(user2.getName());
+    }
+
+    @Test
+    void id로_사용자를_조회할_수_있다() {
+        //given
+        User user = userRepository.save(new User("송재백"));
+
+        //when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when().get("/users/"+user.getId())
+                .then().log().all()
+                .statusCode(200)
+                .extract();
+
+        UserResponse userResponse = response.jsonPath().getObject(".", UserResponse.class);
+        assertThat(userResponse.id()).isEqualTo(user.getId());
+        assertThat(userResponse.name()).isEqualTo(user.getName());
     }
 }
